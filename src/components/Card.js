a83469f8c17d69c176399dacc17fc961be462a57
NickAbadio <73760368+NickAbadio@users.noreply.github.com>
2021-09-07 22:21:31 -0400

    Finished Tilt Effect on image

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import "./Card.css";
+function Card() {
+  return (
+    <div class="contaienr">
+      <div className="tilt-box-wrap">
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <div class="tilt-box">
+          <strong>Tilt Effect</strong>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
